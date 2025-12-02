02f72dbb6c56a62aee8dd0cb5176217c77bb1d0f
MoondaeGI <ansgur119977@gmail.com>
2025-12-03 00:28:04 +0900

    251203 message model 추가

---
+++ models/message.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from config import Base
+
+class Message(Base):
+    __tablename__ = "message"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    person_id = Column(Integer, ForeignKey("person.id"), nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
