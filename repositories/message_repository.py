80488475d9e82e3c08dcbd082f6c09e6c5aeaaa0
MoondaeGI <ansgur119977@gmail.com>
2025-12-03 22:42:56 +0900

    251203 message reposirotry 변경

--- repositories/message_repository.py
+++ repositories/message_repository.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 from sqlalchemy.orm import Session
-from models.person import Person
+from models.message import Message
 from repositories.base import BaseRepository
 
 
@@ -9,7 +9,7 @@ class MessageRepository(BaseRepository[Message]):
     def __init__(self, db: Session):
         super().__init__(Message, db)
     
-    
+
     def get_all(self) -> List[Message]:
         return self.db.query(Message).all()
 
